4bbe14495613cc7b5f3e44aa96769c23041526ef
Samba <kasojusambashiva@gmail.com>
2024-04-05 17:46:44 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,22 @@
+package Shiva693.git_demo;
+
+public class Array {
+
+	public class Testarray {
+	
+		 {  
+			   String s1="Sachin";  
+			   
+			   String s2="Sachin";  
+			   
+			   String s3="Sachin";  
+			   
+			   String s4="Saurav";  
+			   
+			   System.out.println(s1.compareTo(s2));
+			
+			   System.out.println(s1.compareTo(s3)); 
+			   
+			   System.out.println(s1.compareTo(s4));
+			 }  
+	}}
